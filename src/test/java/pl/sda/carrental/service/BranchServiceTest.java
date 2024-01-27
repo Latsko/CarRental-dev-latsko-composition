d0cc623985ec21b82e1cbe810257241ae3312b68
Stanislav Latsko <01latsko10@gmail.com>
2024-01-27 13:43:50 +0100

    commit - more unit tests for addBranch method

--- src/test/java/pl/sda/carrental/service/BranchServiceTest.java
+++ src/test/java/pl/sda/carrental/service/BranchServiceTest.java
@@ -3,25 +3,17 @@ package pl.sda.carrental.service;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
 import pl.sda.carrental.exceptionHandling.ObjectNotFoundInRepositoryException;
-import pl.sda.carrental.model.Branch;
-import pl.sda.carrental.model.CarRental;
-import pl.sda.carrental.model.Employee;
+import pl.sda.carrental.model.*;
 import pl.sda.carrental.repository.*;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
-import static java.util.Optional.empty;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 import static reactor.core.publisher.Mono.when;
@@ -42,17 +34,33 @@ class BranchServiceTest {
     //@InjectMocks
     private BranchService branchService = new BranchService(branchRepositoryMock, carRepositoryMock, employeeRepositoryMock, reservationRepositoryMock, carRentalRepositoryMock);
 
-    private Branch branch;
     private CarRental carRental;
+    private Branch branch;
+    private Branch branchWithData;
     private Employee manager;
+    private List<Reservation> reservations;
 
 
     @BeforeEach
     public void setUp() {
-        branch = new Branch();
         carRental = new CarRental();
+        branch = new Branch();
+        branchWithData = new Branch(111L, "name", "address", 1L, new HashSet<>(), new HashSet<>(), new HashSet<>(), carRental, new Revenue());
         manager = new Employee(1L, "", "", null, null);
-
+        reservations  = new ArrayList<>() {
+            {
+                add(new Reservation());
+                add(new Reservation());
+                add(new Reservation());
+            }
+        };
+        for(Reservation reservation : reservations) {
+          reservation.setStartBranch(branchWithData);
+          reservation.setEndBranch(branchWithData);
+        }
+//        reservations.forEach(reservation -> reservation.setStartBranch(branchWithData));
+//        reservations.forEach(reservation -> reservation.setEndBranch(branchWithData));
+        reservations.add(new Reservation());
     }
 
     @Test
@@ -120,4 +128,88 @@ class BranchServiceTest {
         verify(employeeRepositoryMock, never()).save(manager);
         verify(branchRepositoryMock, never()).save(branch);
     }
+
+    @Test
+    void shouldGetAllBranches() {
+        //given
+        given(branchRepositoryMock.findAll()).willReturn(List.of(branch, branch, branch));
+
+        //when
+        List<Branch> allBranches = branchService.getAllBranches();
+
+        //then
+        assertThat(allBranches).isNotNull();
+        assertThat(allBranches).isNotEmpty();
+        assertThat(allBranches.size()).isEqualTo(3);
+    }
+
+    @Test
+    void shouldGetAllBranchesWithEmptyList() {
+        //given
+        given(branchRepositoryMock.findAll()).willReturn(new ArrayList<>());
+
+        //when
+        List<Branch> allBranches = branchService.getAllBranches();
+
+        //then
+        assertThat(allBranches).isNotNull();
+        assertThat(allBranches.size()).isEqualTo(0);
+        assertThat(allBranches).isEmpty();
+    }
+
+    @Test
+    void shouldEditBranchById() {
+        //given
+        given(branchRepositoryMock.findById(anyLong())).willReturn(Optional.of(branch));
+        given(branchRepositoryMock.save(branch)).willReturn(branch);
+
+        //when
+        Branch modified = branchService.editBranch(1L, branchWithData);
+
+        //then
+        assertThat(modified).isNotNull();
+        assertThat(modified.getName()).isEqualTo("name");
+        assertThat(modified.getAddress()).isEqualTo("address");
+    }
+
+    @Test
+    void shouldGetBranchById() {
+        //given
+        branch.setBranchId(1L);
+        given(branchRepositoryMock.findById(1L)).willReturn(Optional.of(branch));
+
+        //when
+        Branch foundBranch = branchService.getById(1L);
+
+        //then
+        assertThat(foundBranch).isNotNull();
+        assertThat(foundBranch.getBranchId()).isEqualTo(1L);
+    }
+
+    @Test
+    void shouldNotGetBranchById() {
+        //given
+        //when
+        ThrowableAssert.ThrowingCallable callable = () -> branchService.getById(1L);
+
+        //then
+        assertThatThrownBy(callable)
+                .isInstanceOf(ObjectNotFoundInRepositoryException.class)
+                .hasMessage("No branch under ID #1");
+    }
+
+    @Test
+    void shouldRemoveBranchWithAllAssociatedReservations() {
+        //given
+        given(branchRepositoryMock.findById(anyLong())).willReturn(Optional.of(branchWithData));
+        given(reservationRepositoryMock.findAll()).willReturn(reservations);
+
+        //when
+        branchService.removeBranch(1L);
+
+        //then
+        verify(reservationRepositoryMock, times(1)).deleteAll(reservations);
+        verify(branchRepositoryMock, times(1)).deleteById(1L);
+    }
+
 }
