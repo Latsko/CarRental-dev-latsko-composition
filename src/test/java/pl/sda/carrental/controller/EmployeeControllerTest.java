277b4a1f65a9c94c01079943fe6e54c926665cbf
Stanislav Latsko <01latsko10@gmail.com>
2024-02-29 22:46:23 +0100

    commit - EmployeeController test cases added

--- src/test/java/pl/sda/carrental/controller/EmployeeControllerTest.java
+++ src/test/java/pl/sda/carrental/controller/EmployeeControllerTest.java
@@ -26,8 +26,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 
-
-import static org.junit.jupiter.api.Assertions.*;
 import static org.hamcrest.CoreMatchers.is;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
